8c400371164109b3e5b557f1a6f77fc835c1b150
jkevinfg <jkfloresg@uni.pe>
2022-04-21 22:17:21 -0500

    get student id

--- src/routes/students.js
+++ src/routes/students.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connection = require('../config/db')
 
 
 function studentsApi(app) {
@@ -6,9 +7,30 @@ function studentsApi(app) {
     const router = express.Router();
     app.use('/api/students', router);
     
+    //get students
     router.get('/',(req,res) => {
-
+      const sql = 'select * from students';
+      connection.query(sql,(err,rows) => {
+        if(err) throw err;
+        else {
+          res.json(rows)
+        }
+      });
     });
+
+    //get student id
+    router.get('/:studentId',(req,res) => {
+      const {studentId} = req.params;
+      const sql = 'select * from students where id_student = ?';
+      connection.query(sql,[studentId],(err,rows) => {
+        if(err) throw err;
+        else {
+          res.json(rows)
+        }
+      });
+    });
+
+
 
 
   }
